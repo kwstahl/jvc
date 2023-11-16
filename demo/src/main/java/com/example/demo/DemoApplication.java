dc65570e632a3c4d9a0ec650cbdd72be3891dd5c
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2023-11-16 21:50:06 +0000

    modified:   demo/src/main/java/com/example/demo/DemoApplication.java

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -6,6 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
+    @RequestMapping("/")
+    String home(){
+        return "Hello World!";
+    }
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}

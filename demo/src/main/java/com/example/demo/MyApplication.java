156cae5281754bf7692351db46d1610aff41c9b4
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2023-11-16 21:42:16 +0000

    modified:   demo/pom.xml 	new file:   demo/src/main/java/com/example/demo/MyApplication.java

---
+++ demo/src/main/java/com/example/demo/MyApplication.java
@@ -0,0 +1,13 @@
+@RestController
+@SpringBootApplication
+
+public class MyApplication{
+    @RequestMapping("/")
+    String home(){
+        return "Hello World!";
+    }
+
+    public static void main(String[] args){
+        SpringApplication.run(MyApplication.class, args);
+    }
+}

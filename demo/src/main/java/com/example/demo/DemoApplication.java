7612df985becb167d99505c25d85e38e4d69cd0c
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2023-11-16 22:05:31 +0000

    modified:   demo/src/main/java/com/example/demo/DemoApplication.java

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -7,13 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @SpringBootApplication
 public class DemoApplication {
-
-    @RequestMapping("/")
-    String home(){
-        return "Hello World!";
-    }
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
 }
